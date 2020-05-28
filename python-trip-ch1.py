c97925487966fd64d2bcdbdfdf9d8f84f7ef1c00
ryan lai <ryan20151016@gmail.com>
2020-05-28 23:51:30 +0800

    Add files via upload

---
+++ python-trip-ch1.py
@@ -0,0 +1,12 @@
+#第一章
+score = [70, 80, 90]
+min_grade = None
+max_grade = None
+average_grade = None
+print("國文:%d"%score[0])
+# min_grade_key = score.index(min(score))
+# print("最低分:%d"%score[min_grade_key])
+min_grade = min(score)
+print("最低分:%d"%min_grade)
+
+
